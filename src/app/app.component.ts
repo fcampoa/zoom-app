13c0d6846426d478d46cf2f2c0fd38a8514d305b
felix campoa <felixcampoa@gmail.com>
2021-08-27 15:09:06 -0700

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ ZoomMtg.i18n.reload('en-US');
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
---
+++ src/app/model/IMeeting.ts
@@ -0,0 +1,10 @@
+export interface IMeeting {
+    signatureEndpoint?: string;
+    apiKey?: string;
+    meetingNumber?: string;
+    role?: number;
+    leaveUrl?: string;
+    userName?: string;
+    userEmail?: string;
+    passWord?: string;
+}
